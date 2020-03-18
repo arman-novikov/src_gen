0b612a96af8d516dd7180595fe772119585d8ae9
armnov <arm.nov@ya.ru>
2020-03-18 13:52:03 +0300

    src_gen.py -> main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from _utils import work_dir_creator
+from configer import config_creator
+from commoner import common_creator
+from proper import props_creator
+from mainer import main_creator
+from initer import ini_creator
+from scripter import script_creator
+from readmer import readme_creator
+
+work_dir_creator()
+config_creator()
+common_creator()
+props_creator();
+main_creator();
+ini_creator()
+script_creator()
+readme_creator()
